c58764f233734e87b5cad51504911aeb4c5e4ecf
eduard <cf17eduard.corral@iesjoandaustria.org>
2018-10-30 20:48:07 +0100

    Ex del 1 al 5

---
+++ Eduard Corral/Ex1.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class Ex1 {
+    public static void main(String[] args) {
+        String dir = "/home/edu/Descargas";//directorio descargas
+        File f = new File(dir);
+        String[] archivos = f.list();
+        System.out.printf("Ficheros en el directorio actual: %d %n", archivos.length);
+        for (int i = 0; i < archivos.length; i++) {
+            File f2 = new File(f, archivos[i]);
+            System.out.printf("Nombre: %s, es fichero?: %b, es directorio?: %b %n", archivos[i],
+                    f2.isFile(), f2.isDirectory());
+        }
+    }
+}
---
+++ Eduard Corral/Ex2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class Ex2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Introdueze la ruta");
+        String texto = reader.readLine();
+        String dir = texto; //directorio
+        File f = new File(dir);
+        String[] archivos = f.list();
+        System.out.printf("Ficheros en el directorio actual: %d %n", archivos.length);
+        for (int i = 0; i < archivos.length; i++) {
+            File f2 = new File(f, archivos[i]);
+            System.out.printf("Nombre: %s, es fichero?: %b, es directorio?: %b %n", archivos[i],
+                    f2.isFile(), f2.isDirectory());
+        }
+    }
+}
