b163e919f2cc305a3991ff50633adb84b4df6395
eduard <cf17eduard.corral@iesjoandaustria.org>
2019-02-18 01:52:45 +0100

    hibernatev parte

---
+++ Eduard Corral/Uf2/Hibernate V/Ex4.java
@@ -0,0 +1,26 @@
+import primero.*;
+import java.util.Iterator;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+/**Calcula el salario medio de todos los empleados. (Ayuda: busca en el manual
+de HQL cómo utilizar las funciones de agregación. Verás que es lo mismo
+que en SQL)**/
+public class Ex4 {
+	public static void main(String[] args){
+		SessionFactory sesion = HibernateUtil.getSessionFactory();
+		Session session = sesion.openSession();
+		Query q = session.createQuery("select avg(salario) from Emple");
+		List <Emple> lista =q.list();
+		Iterator <Emple> iter = lista.iterator();
+		
+		
+		while (iter.hasNext()){
+			System.out.println("Saliro medio: "+iter.next());
+
+					
+		}		session.close();
+		System.exit(0);
+	}
+}
