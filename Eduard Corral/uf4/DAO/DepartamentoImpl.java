ff9a1d57cbe50f596331624363d7be4feb4e11c1
eduard <cf17eduard.corral@iesjoandaustria.org>
2019-05-31 14:21:10 +0200

    añadidos metos a dao

--- Eduard Corral/uf4/DAO/DepartamentoImpl.java
+++ Eduard Corral/uf4/DAO/DepartamentoImpl.java
@@ -2,6 +2,10 @@ package dao;
 
 import org.neodatis.odb.ODB;
 import org.neodatis.odb.ODBFactory;
+import org.neodatis.odb.Objects;
+import org.neodatis.odb.core.query.IQuery;
+import org.neodatis.odb.core.query.criteria.Where;
+import org.neodatis.odb.impl.core.query.criteria.CriteriaQuery;
 
 public class DepartamentoImpl implements DepartamentoDAO {
 	private static ODB odb;
@@ -28,20 +32,51 @@ public class DepartamentoImpl implements DepartamentoDAO {
 	@Override
 	public boolean EliminarDep(int deptno) {
 		// TODO Auto-generated method stub
-		return false;
+
+		try {
+			IQuery select = new CriteriaQuery(Departamento.class, Where.equal("deptno", deptno));
+			Objects<Departamento> departamento = odb.getObjects(select);				
+			Departamento departEliminar = (Departamento) departamento.getFirst();
+			this.odb.delete(departEliminar);
+			return true;
+		}catch(Exception e) {
+			return false;
+		}
 	}
 
 	@Override
 	public boolean ModificarDep(int deptn, Departamento departamento) {
 		// TODO Auto-generated method stub
-		return false;
+		Departamento departamentoEdit = ConsultarDep(deptn);
+		if (departamentoEdit!=null) {
+
+			departamentoEdit.setDeptno(departamento.getDeptno());
+			departamentoEdit.setDnombre(departamento.getDnombre());
+			departamentoEdit.setLoc(departamento.getLoc());
+			this.odb.store(departamentoEdit);
+
+			return true;
+		}else {
+			return false;
+		}
 	}
 
 	@Override
 	public Departamento ConsultarDep(int deptno) {
 		// TODO Auto-generated method stub
-		return null;
+		IQuery select = new CriteriaQuery(Departamento.class,Where.equal("deptno", deptno));
+		Objects<Departamento> departamento = odb.getObjects(select);	
+		if(departamento!=null && departamento.size()>0) {
+			Departamento departConsulta = (Departamento) departamento.getFirst();
+
+			return departConsulta;	
+		}
+		else {
+			System.out.println("El departamento consultado no existe");
+			return null;
+		}
 	}
+
 	public static ODB getOdb() {
 		return odb;
 	}
